5ecdb86c740afa333a250e5d348a5e4c7c7d9160
Sergio-e <33036058+Sergio-e@users.noreply.github.com>
2024-10-31 17:12:26 -0600

    dashlane-adapter

--- lib/kamal/secrets/adapters/dashlane.rb
+++ lib/kamal/secrets/adapters/dashlane.rb
@@ -1,30 +1,34 @@
 class Kamal::Secrets::Adapters::Dashlane < Kamal::Secrets::Adapters::Base
   private
-    def login(account)
-      unless loggedin?(account)
-        `dcli sync`
-        raise RuntimeError, "Failed to login to Dashlane" unless $?.success?
+
+  def login(_account)
+    `dcli sync`
+    raise "Failed to login to Dashlane" unless $?.success?
+  end
+
+  def fetch_secrets(secrets, account:, session:)
+    items = `dcli secret #{secrets.map(&:shellescape).join(" ")} -o json`
+    raise "Could not read #{secrets} from Dashlane" unless $?.success?
+
+    items = JSON.parse(items)
+
+    {}.tap do |results|
+      items.each do |item|
+        results[item["title"]] = item["content"]
+      end
+
+      if (missing_items = secrets - results.keys).any?
+        raise "Could not find #{missing_items.join(", ")} in Dashlane"
       end
     end
+  end
 
-    def loggedin?(account)
-      `dcli accounts whoami`.strip == account
-    end
+  def check_dependencies!
+    raise "Dashlane CLI is not installed" unless cli_installed?
+  end
 
-    def fetch_secrets(secrets, account:, session:)
-      items = `dcli secret #{secrets.map(&:shellescape).join(" ")} -o json`
-      raise RuntimeError, "Could not read #{secrets} from Dashlane" unless $?.success?
-
-      items = JSON.parse(items)
-
-      {}.tap do |results|
-        items.each do |item|
-          results[item["title"]] = item["content"]
-        end
-
-        if (missing_items = secrets - results.keys).any?
-          raise RuntimeError, "Could not find #{missing_items.join(", ")} in Dashlane"
-        end
-      end
-    end
+  def cli_installed?
+    `dcli --version 2> /dev/null`
+    $?.success?
+  end
 end
--- test/secrets/dashlane_adapter_test.rb
+++ test/secrets/dashlane_adapter_test.rb
@@ -5,11 +5,20 @@ class DashlaneAdapterTest < SecretAdapterTestCase
     `true` # Ensure $? is 0
   end
 
+  test "fetch without CLI installed" do
+    stub_ticks_with("dcli -v", succeed: false)
+
+    error = assert_raises RuntimeError do
+      JSON.parse(shellunescape(run_command("fetch", "SECRET1")))
+    end
+    assert_equal "Dashlane CLI is not installed", error.message
+  end
+
   test "fetch" do
-    stub_ticks.with("dcli accounts whoami").returns("email@example.com")
+    stub_ticks.with("dcli sync").returns("email@example.com")
 
     stub_ticks
-      .with("dcli secret SECRET1 FOLDER1/FSECRET1 FOLDER1/FSECRET2 -o json")
+      .with("dcli secret SECRET1 SECRET2 SECRET3 -o json")
       .returns(<<~JSON)
         [
           {
@@ -26,8 +35,8 @@ class DashlaneAdapterTest < SecretAdapterTestCase
           },
           {
             "id": "1234567891234567891",
-            "title": "FOLDER1/FSECRET1",
-            "content": "fsecret1",
+            "title": "SECRET2",
+            "content": "secret2",
             "creationDatetime": "1724926084",
             "lastBackupTime": "1724926635",
             "lastUse": "123456789",
@@ -38,8 +47,8 @@ class DashlaneAdapterTest < SecretAdapterTestCase
           },
           {
             "id": "1234567891234567891",
-            "title": "FOLDER1/FSECRET2",
-            "content": "fsecret2",
+            "title": "SECRET3",
+            "content": "secret3",
             "creationDatetime": "1724926084",
             "lastBackupTime": "1724926635",
             "lastUse": "123456789",
@@ -51,28 +60,28 @@ class DashlaneAdapterTest < SecretAdapterTestCase
         ]
       JSON
 
-    json = JSON.parse(shellunescape(run_command("fetch", "SECRET1", "FOLDER1/FSECRET1", "FOLDER1/FSECRET2")))
+    json = JSON.parse(shellunescape(run_command("fetch", "SECRET1", "SECRET2", "SECRET3")))
 
     expected_json = {
-      "SECRET1"=>"secret1",
-      "FOLDER1/FSECRET1"=>"fsecret1",
-      "FOLDER1/FSECRET2"=>"fsecret2"
+      "SECRET1" => "secret1",
+      "SECRET2" => "secret2",
+      "SECRET3" => "secret3"
     }
 
     assert_equal expected_json, json
   end
 
   test "fetch with from" do
-    stub_ticks.with("dcli accounts whoami").returns("email@example.com")
+    stub_ticks.with("dcli sync").returns("email@example.com")
 
     stub_ticks
-      .with("dcli secret FOLDER1/FSECRET1 FOLDER1/FSECRET2 -o json")
+      .with("dcli secret SECRET1 APP/SECRET1 -o json")
       .returns(<<~JSON)
         [
           {
             "id": "1234567891234567891",
-            "title": "FOLDER1/FSECRET1",
-            "content": "fsecret1",
+            "title": "SECRET1",
+            "content": "secret1",
             "creationDatetime": "1724926084",
             "lastBackupTime": "1724926635",
             "lastUse": "123456789",
@@ -83,8 +92,8 @@ class DashlaneAdapterTest < SecretAdapterTestCase
           },
           {
             "id": "1234567891234567891",
-            "title": "FOLDER1/FSECRET2",
-            "content": "fsecret2",
+            "title": "APP/SECRET2",
+            "content": "secret2",
             "creationDatetime": "1724926084",
             "lastBackupTime": "1724926635",
             "lastUse": "123456789",
@@ -96,57 +105,44 @@ class DashlaneAdapterTest < SecretAdapterTestCase
         ]
       JSON
 
-    json = JSON.parse(shellunescape(run_command("fetch", "--from", "FOLDER1", "FSECRET1", "FSECRET2")))
+    json = JSON.parse(shellunescape(run_command("fetch", "--from", "SECRET1", "APP/SECRET2")))
 
     expected_json = {
-      "FOLDER1/FSECRET1"=>"fsecret1",
-      "FOLDER1/FSECRET2"=>"fsecret2"
-    }
-
-    assert_equal expected_json, json
-  end
-
-  test "fetch with signin" do
-    stub_ticks_with("dcli accounts whoami", succeed: false).returns("")
-    stub_ticks_with("dcli sync", succeed: true).returns("")
-    stub_ticks.with("dcli secret SECRET1 -o json").returns(single_item_json)
-
-    json = JSON.parse(shellunescape(run_command("fetch", "SECRET1")))
-
-    expected_json = {
-      "SECRET1"=>"secret1"
+      "SECRET1" => "secret1",
+      "APP/SECRET2" => "secret2"
     }
 
     assert_equal expected_json, json
   end
 
   private
-    def run_command(*command)
-      stdouted do
-        Kamal::Cli::Secrets.start \
-          [ *command,
-            "-c", "test/fixtures/deploy_with_accessories.yml",
-            "--adapter", "dashlane",
-            "--account", "email@example.com" ]
-      end
-    end
 
-    def single_item_json
-      <<~JSON
-        [
-          {
-            "id": "1234567891234567891",
-            "title": "SECRET1",
-            "content": "secret1",
-            "creationDatetime": "1724926635",
-            "lastBackupTime": "1724926635",
-            "lastUse": "1724926635",
-            "localeFormat": "UNIVERSAL",
-            "spaceId": "123456",
-            "userModificationDatetime": "1724926635",
-            "secured": "false"
-          }
-        ]
-      JSON
+  def run_command(*command)
+    stdouted do
+      Kamal::Cli::Secrets.start \
+        [*command,
+          "-c", "test/fixtures/deploy_with_accessories.yml",
+          "--adapter", "dashlane",
+          "--account", "email@example.com"]
     end
+  end
+
+  def single_item_json
+    <<~JSON
+      [
+        {
+          "id": "1234567891234567891",
+          "title": "SECRET1",
+          "content": "secret1",
+          "creationDatetime": "1724926635",
+          "lastBackupTime": "1724926635",
+          "lastUse": "1724926635",
+          "localeFormat": "UNIVERSAL",
+          "spaceId": "123456",
+          "userModificationDatetime": "1724926635",
+          "secured": "false"
+        }
+      ]
+    JSON
+  end
 end
